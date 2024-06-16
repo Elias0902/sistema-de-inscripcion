b14198e60892385be91e78fb735272c72657463e
Elias0902 <eliasarmas0902@gmail.com>
2024-06-16 10:10:38 -0400

    Version 1

---
+++ core/configGeneral.php
@@ -0,0 +1,22 @@
+<?php
+	const SERVERURL = "http://localhost/sistemaGualdron/";
+
+	const COMPANY = "sistemaGualdron";
+
+	/*====================================
+	=            Zona horaria            =
+	====================================*/
+	date_default_timezone_set("America/Caracas");
+
+	/**
+		Zonas horarias:
+		- America/El_Salvador
+		- America/Costa_Rica
+		- America/Guatemala
+		- America/Puerto_Rico
+		- America/Panama
+		- Europe/Madrid
+
+		Más zonas, visita http://php.net/manual/es/timezones.php
+
+	/*=====  End of Zona horaria  ======*/
